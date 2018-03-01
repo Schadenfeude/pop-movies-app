4525f82547f04e0ee443da4f052a0fb982dd73d7
Daniel Milkov <daniel.milkov@wacom.eu>
2018-03-01 14:05:50 +0200

    first commit

---
+++ app/src/main/java/com/itrided/android/popularmovies/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.itrided.android.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+//region API Methods
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+//endregion API Methods
+}
